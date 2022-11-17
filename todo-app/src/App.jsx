52b4a306c98b76e1f665c3029813a728662a19c5
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-17 13:41:29 +0100

    h

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import TodosList from "./TodosList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const [todos, setTodo] = useState([
+        {
+            text: "clean kitchen",
+            id: 0,
+        },
+        {
+            text: "learn programming",
+            id: 1,
+        },
+    ]);
+    const handleClickDelete = (id) => {
+        const newTodos = todos.filter((todo) => todo.id !== id);
+        setTodo(newTodos);
+    };
+    return (
+        <div className="App">
+            <TodosList
+                todos={todos}
+                title="Todo ithem:"
+                handleClickDelete={handleClickDelete}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
